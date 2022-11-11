31e24dd14c28a66de3cbc418cab51e1ba4dc7d40
Sebastian Chavez Dev <piixelboy@gmail.com>
2022-11-10 20:08:44 -0500

    [add] animation class

--- Assets/Ui/UiManager.cs
+++ Assets/Ui/UiManager.cs
@@ -8,18 +8,28 @@ using UnityEngine.UI;
 public class UiManager : MonoBehaviour
 {
     [SerializeField] private Text points;
-
+    public Animator _animator;
+    public bool showMenu;
+    public GameObject WaponSelectorPanel;
 
     
     // Start is called before the first frame update
     void Start()
     {
-       
+        showMenu = false;
+        _animator = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+       
+    }
+
+    public void showMenuPanel()
+    {
+        
+        _animator.SetBool("ShowMenu", true);
         
     }
 
